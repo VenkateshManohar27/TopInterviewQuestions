04d6e0e83fcd7f9020cee447504e9d24fa7d063e
veenkateshmanohar27 <venkatesh.manohar@gmail.com>
2025-08-12 07:59:16 -0400

    Top interview easy questions practice

---
+++ src/main/java/com/ven/sort/MergeSortedArray.java
@@ -0,0 +1,82 @@
+package com.ven.sort;
+/**
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+ * <p>
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ * <p>
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ * <p>
+ *
+ *
+ * Example 1:
+ * <p>
+ * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * Output: [1,2,2,3,5,6]
+ * Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+ * The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+ * Example 2:
+ * <p>
+ * Input: nums1 = [1], m = 1, nums2 = [], n = 0
+ * Output: [1]
+ * Explanation: The arrays we are merging are [1] and [].
+ * The result of the merge is [1].
+ * Example 3:
+ * <p>
+ * Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+ * Output: [1]
+ * Explanation: The arrays we are merging are [] and [1].
+ * The result of the merge is [1].
+ * Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+ * <p>
+ *
+ * Constraints:
+ * <p>
+ * nums1.length == m + n
+ * nums2.length == n
+ * 0 <= m, n <= 200
+ * 1 <= m + n <= 200
+ * -109 <= nums1[i], nums2[j] <= 109
+ * <p>
+ *
+ * Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+ */
+public class MergeSortedArray {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] nums3 = new int[m];
+
+        for(int i = 0; i < m; i++) {
+            nums3[i] = nums1[i];
+        }
+
+        int i = 0, j = 0;
+        int in = 0;
+        while(i < m && j < n) {
+            if(nums3[i] <= nums2[j]){
+                nums1[in++] = nums3[i++];
+            }else{
+                nums1[in++] = nums2[j++];
+            }
+        }
+        while(i < m) {
+            nums1[in++] = nums3[i++];
+        }
+
+        while(j < n) {
+            nums1[in++] = nums2[j++];
+        }
+
+    }
+
+    public static void main(String[] args) {
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+        mergeSortedArray.merge(nums1, m, nums2, n);
+        for(int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
+}
---
+++ src/main/java/com/ven/tree/ConvertSortedArraytoBinarySearchTree.java
@@ -0,0 +1,56 @@
+package com.ven.tree;
+
+/**
+ * Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+ * <p>
+ *
+ *
+ * Example 1:
+ * <p>
+ *
+ * Input: nums = [-10,-3,0,5,9]
+ * Output: [0,-3,9,-10,null,5]
+ * Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+ * <p>
+ * Example 2:
+ * <p>
+ *
+ * Input: nums = [1,3]
+ * Output: [3,1]
+ * Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+ * <p>
+ *
+ * Constraints:
+ * <p>
+ * 1 <= nums.length <= 104
+ * -104 <= nums[i] <= 104
+ * nums is sorted in a strictly increasing order.
+ */
+public class ConvertSortedArraytoBinarySearchTree {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return generate(0, nums.length - 1, nums);
+    }
+
+    private TreeNode generate( int start, int end, int[] nums) {
+        if(start > end) {
+            return null;
+        }
+        int mid = (start + end) / 2;
+
+        TreeNode node = new TreeNode(nums[mid]);
+
+        node.left = generate(start, mid - 1, nums);
+        node.right = generate(mid+1, end, nums);
+        return node;
+    }
+
+    public static void main(String[] args) {
+        ConvertSortedArraytoBinarySearchTree converter = new ConvertSortedArraytoBinarySearchTree();
+        int[] nums = {-10, -3, 0, 5, 9};
+        TreeNode root = converter.sortedArrayToBST(nums);
+
+        System.out.println("Root of the BST: " + root.val); // Output: 0
+        System.out.println("Left child of root: " + root.left.val); // Output: -3
+        System.out.println("Right child of root: " + root.right.val); // Output: 5
+    }
+}
