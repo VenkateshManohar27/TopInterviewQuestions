0ed9b13d0199b2880f99e67efe22ec6cfc34d830
Venkatesh Manohar <venkatesh.manohar@gmail.com>
2025-08-14 08:55:04 -0400

    Practice aug14 (#4)
    * Top interview easy questions practice
    
    * Top interview easy questions practice
    
    * Top interview easy questions practice

---
+++ src/main/java/com/ven/daily/aug/LargestGoodInteger.java
@@ -0,0 +1,59 @@
+package com.ven.daily.aug;
+
+/**
+ * You are given a string num representing a large integer. An integer is good if it meets the following conditions:
+ * <p>
+ * It is a substring of num with length 3.
+ * It consists of only one unique digit.
+ * Return the maximum good integer as a string or an empty string "" if no such integer exists.
+ * <p>
+ * Note:
+ * <p>
+ * A substring is a contiguous sequence of characters within a string.
+ * There may be leading zeroes in num or a good integer.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: num = "6777133339"
+ * Output: "777"
+ * Explanation: There are two distinct good integers: "777" and "333".
+ * "777" is the largest, so we return "777".
+ * Example 2:
+ * <p>
+ * Input: num = "2300019"
+ * Output: "000"
+ * Explanation: "000" is the only good integer.
+ * Example 3:
+ * <p>
+ * Input: num = "42352338"
+ * Output: ""
+ * Explanation: No substring of length 3 consists of only one unique digit. Therefore, there are no good integers.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 3 <= num.length <= 1000
+ * num only consists of digits.
+ */
+public class LargestGoodInteger {
+    public String largestGoodInteger(String num) {
+        char maxDigit = '\0';
+        char[] chs = num.toCharArray();
+
+        for (int i = 2; i < chs.length; i++) {
+            if (chs[i - 2] == chs[i - 1] && chs[i - 1] == chs[i]) {
+                maxDigit = (char) Math.max(maxDigit, chs[i]);
+            }
+        }
+
+        return maxDigit == '\0' ? "" : new String(new char[]{maxDigit, maxDigit, maxDigit});
+    }
+
+    public static void main(String[] args) {
+        LargestGoodInteger largestGoodInteger = new LargestGoodInteger();
+        String num = "6777133339"; // Example input
+        String result = largestGoodInteger.largestGoodInteger(num);
+        System.out.println("The largest good integer is: " + result); // Output: "777"
+    }
+}
---
+++ src/main/java/com/ven/other/Numberof1Bits.java
@@ -0,0 +1,63 @@
+package com.ven.other;
+
+/**
+ * Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 11
+ * <p>
+ * Output: 3
+ * <p>
+ * Explanation:
+ * <p>
+ * The input binary string 1011 has a total of three set bits.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: n = 128
+ * <p>
+ * Output: 1
+ * <p>
+ * Explanation:
+ * <p>
+ * The input binary string 10000000 has a total of one set bit.
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: n = 2147483645
+ * <p>
+ * Output: 30
+ * <p>
+ * Explanation:
+ * <p>
+ * The input binary string 1111111111111111111111111111101 has a total of thirty set bits.
+ * <p>
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= n <= 231 - 1
+ */
+public class Numberof1Bits {
+    public int hammingWeight(int n) {
+        int count = 0;
+
+        for (int i = 0; i < 31; i++) {
+            if ((n & (1 << i)) != 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Numberof1Bits numberof1Bits = new Numberof1Bits();
+        int n = 11; // Example input
+        int result = numberof1Bits.hammingWeight(n);
+        System.out.println("Number of set bits in " + n + " is: " + result);
+    }
+}
