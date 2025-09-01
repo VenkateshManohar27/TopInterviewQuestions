a26da1b93a1fdc1fda2f67fd7a1b871c5668969e
Venkatesh Manohar <venkatesh.manohar@gmail.com>
2025-09-01 11:17:53 -0400

    Top interview medium questions practice (#25)

---
+++ src/main/java/com/ven/medium/design/TicTacToe.java
@@ -0,0 +1,125 @@
+package com.ven.medium.design;
+
+/**
+ * Assume the following rules are for the tic-tac-toe game on an n x n board between two players:
+ * <p>
+ * A move is guaranteed to be valid and is placed on an empty block.
+ * Once a winning condition is reached, no more moves are allowed.
+ * A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+ * Implement the TicTacToe class:
+ * <p>
+ * TicTacToe(int n) Initializes the object the size of the board n.
+ * int move(int row, int col, int player) Indicates that the player with id player plays at the cell (row, col) of the board. The move is guaranteed to be a valid move, and the two players alternate in making moves. Return
+ * 0 if there is no winner after the move,
+ * 1 if player 1 is the winner after the move, or
+ * 2 if player 2 is the winner after the move.
+ * <p>
+ *
+ * Example 1:
+ * <p>
+ * Input
+ * ["TicTacToe", "move", "move", "move", "move", "move", "move", "move"]
+ * [[3], [0, 0, 1], [0, 2, 2], [2, 2, 1], [1, 1, 2], [2, 0, 1], [1, 0, 2], [2, 1, 1]]
+ * Output
+ * [null, 0, 0, 0, 0, 0, 0, 1]
+ * <p>
+ * Explanation
+ * TicTacToe ticTacToe = new TicTacToe(3);
+ * Assume that player 1 is "X" and player 2 is "O" in the board.
+ * ticTacToe.move(0, 0, 1); // return 0 (no one wins)
+ * |X| | |
+ * | | | |    // Player 1 makes a move at (0, 0).
+ * | | | |
+ * <p>
+ * ticTacToe.move(0, 2, 2); // return 0 (no one wins)
+ * |X| |O|
+ * | | | |    // Player 2 makes a move at (0, 2).
+ * | | | |
+ * <p>
+ * ticTacToe.move(2, 2, 1); // return 0 (no one wins)
+ * |X| |O|
+ * | | | |    // Player 1 makes a move at (2, 2).
+ * | | |X|
+ * <p>
+ * ticTacToe.move(1, 1, 2); // return 0 (no one wins)
+ * |X| |O|
+ * | |O| |    // Player 2 makes a move at (1, 1).
+ * | | |X|
+ * <p>
+ * ticTacToe.move(2, 0, 1); // return 0 (no one wins)
+ * |X| |O|
+ * | |O| |    // Player 1 makes a move at (2, 0).
+ * |X| |X|
+ * <p>
+ * ticTacToe.move(1, 0, 2); // return 0 (no one wins)
+ * |X| |O|
+ * |O|O| |    // Player 2 makes a move at (1, 0).
+ * |X| |X|
+ * <p>
+ * ticTacToe.move(2, 1, 1); // return 1 (player 1 wins)
+ * |X| |O|
+ * |O|O| |    // Player 1 makes a move at (2, 1).
+ * |X|X|X|
+ * <p>
+ *
+ * Constraints:
+ * <p>
+ * 2 <= n <= 100
+ * player is 1 or 2.
+ * 0 <= row, col < n
+ * (row, col) are unique for each different call to move.
+ * At most n2 calls will be made to move.
+ * <p>
+ *
+ * Follow-up: Could you do better than O(n2) per move() operation?
+ */
+public class TicTacToe {
+    private int N;
+    private int[] rows;
+    private int[] cols;
+    private int diagonal;
+    private int antidiagonal;
+
+    public TicTacToe(int n) {
+        this.rows = new int[n];
+        this.cols = new int[n];
+        this.diagonal = 0;
+        this.antidiagonal = 0;
+        this.N = n;
+    }
+
+    public int move(int row, int col, int player) {
+        int value = (player == 1) ? 1 : -1;
+
+        rows[row] += value;
+        cols[col] += value;
+        if (row == col) {
+            diagonal += value;
+        }
+
+        if (col == (this.N - row - 1)) {
+            antidiagonal += value;
+        }
+
+        if ((Math.abs(rows[row]) == this.N) ||
+                (Math.abs(cols[col]) == this.N) ||
+                (Math.abs(diagonal) == this.N) ||
+                (Math.abs(antidiagonal) == this.N)) {
+            return player;
+
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        TicTacToe ticTacToe = new TicTacToe(3);
+        System.out.println(ticTacToe.move(0, 0, 1)); // return 0 (no one wins)
+        System.out.println(ticTacToe.move(0, 2, 2)); // return 0 (no one wins)
+        System.out.println(ticTacToe.move(2, 2, 1)); // return 0 (no one wins)
+        System.out.println(ticTacToe.move(1, 1, 2)); // return 0 (no one wins)
+        System.out.println(ticTacToe.move(2, 0, 1)); // return 0 (no one wins)
+        System.out.println(ticTacToe.move(1, 0, 2)); // return 0 (no one wins)
+        System.out.println(ticTacToe.move(2, 1, 1)); // return 1 (player 1 wins)
+    }
+}
+
